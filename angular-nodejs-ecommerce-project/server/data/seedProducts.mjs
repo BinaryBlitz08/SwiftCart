83a14a74f061f69832ed2225da0a21af5692c371
BinaryBlitz08 <bhavya.bharadwaj.24031@iitgoa.ac.in>
2025-07-26 12:37:56 +0530

    Update seedProducts.mjs

--- angular-nodejs-ecommerce-project/server/data/seedProducts.mjs
+++ angular-nodejs-ecommerce-project/server/data/seedProducts.mjs
@@ -360,7 +360,7 @@ const products = [
 
 const seedProducts = async () => {
   try {
-    await mongoose.connect("mongodb+srv://binaryblitz08:mauli@cluster0.byuduhy.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
+    await mongoose.connect("mongodb+srv://binaryblitz08:mauli@cluster0.byuduhy.mongodb.net/swiftcart?retryWrites=true&w=majority&appName=Cluster0");
     await Product.deleteMany();
     await Product.insertMany(products);
     console.log(" Products seeded successfully!");
